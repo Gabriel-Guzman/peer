f972fe1198323584807a2af9ef22b01ebab45698
Fernando Quintero <ferdanielq@outlook.com>
2017-02-25 15:40:31 -0500

    some routing

--- peer/routes/about.js
+++ peer/routes/about.js
@@ -1,1 +1,9 @@
-﻿
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/about', function (req, res, next) {
+    res.sendfile('../public/about.html');
+});
+
+module.exports = router;
